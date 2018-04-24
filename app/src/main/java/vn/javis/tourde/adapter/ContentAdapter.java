d49023cb58a0bc9ff45a624a3710637948602328
nguyen van dien <anhdien2210@gmail.com>
2018-04-24 07:58:44 +0700

    Update search course

--- app/src/main/java/vn/javis/tourde/adapter/ContentAdapter.java
+++ app/src/main/java/vn/javis/tourde/adapter/ContentAdapter.java
@@ -1,4 +1,5 @@
 package vn.javis.tourde.adapter;
+
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,10 +8,13 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.android.volley.toolbox.ClearCacheRequest;
+
 import java.util.HashMap;
 import java.util.List;
 
 import vn.javis.tourde.R;
+import vn.javis.tourde.model.Data;
 
 
 public class ContentAdapter extends RecyclerView.Adapter<ContentAdapter.ViewHolder> {
@@ -24,23 +28,26 @@ public class ContentAdapter extends RecyclerView.Adapter<ContentAdapter.ViewHold
         this.contentList = contentList;
         this.onClickItem = onClickItem;
     }
+
     @Override
     public ContentAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_content, parent, false);
-        ViewHolder viewHolder = new ViewHolder(view);
+        View view = LayoutInflater.from( parent.getContext() ).inflate( R.layout.item_content, parent, false );
+        ViewHolder viewHolder = new ViewHolder( view );
         return viewHolder;
     }
+
     @Override
     public void onBindViewHolder(ContentAdapter.ViewHolder holder, int position) {
 
-        String content = contentList.get(position);
+        String content = contentList.get( position );
 
-        holder.tv_content.setText(content);
+        holder.tv_content.setText( content );
 //        holder.itemView.setOnClickListener(holder);
 //        holder.imv_mark.setTag(position);
-        mapItemView.put(position, holder.imv_mark);
-        if (position == 0 && isShowMark()) holder.imv_mark.setVisibility(View.VISIBLE);
+        mapItemView.put( position, holder.imv_mark );
+        if (position == 0 && isShowMark()) holder.imv_mark.setVisibility( View.VISIBLE );
     }
+
     @Override
     public int getItemCount() {
         return contentList.size();
@@ -48,26 +55,33 @@ public class ContentAdapter extends RecyclerView.Adapter<ContentAdapter.ViewHold
 
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView tv_content;
-        ImageView imv_mark;
+        private ImageView imv_mark;
         RelativeLayout rlt_mark;
 
         public ViewHolder(View itemView) {
-            super(itemView);
-            tv_content = itemView.findViewById( R.id.tv_content);
-            imv_mark = itemView.findViewById(R.id.imv_mark);
-            rlt_mark = itemView.findViewById(R.id.rlt_mark);
-            itemView.setOnClickListener(this);
+            super( itemView );
+            tv_content = itemView.findViewById( R.id.tv_content );
+            imv_mark = itemView.findViewById( R.id.imv_mark );
+            rlt_mark = itemView.findViewById( R.id.rlt_mark );
+            itemView.setOnClickListener( this );
         }
+
         @Override
         public void onClick(View v) {
-            if (onClickItem != null) onClickItem.onClick(getAdapterPosition(), v);
-            mapItemView.put(getAdapterPosition(), imv_mark);
-            imv_mark.setVisibility(View.VISIBLE);
+            if (imv_mark.getVisibility() == View.GONE) {
+                if (onClickItem != null) onClickItem.onClick( getAdapterPosition(),true, v );
+                //  mapItemView.put( getAdapterPosition(), imv_mark );
+                imv_mark.setVisibility( View.VISIBLE );
+            } else if (imv_mark.getVisibility() == View.VISIBLE) {
+                imv_mark.setVisibility( View.GONE );
+                if (onClickItem != null) onClickItem.onClick( getAdapterPosition(), false, v );
+
+            }
         }
     }
 
     public interface OnClickItem {
-        void onClick(int position, View view);
+        void onClick(int position, boolean isPick, View view);
     }
 
     public boolean isShowMark() {
