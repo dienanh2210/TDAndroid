c37c2458a95d0c98377412d7edc3aa04a132285b
bang lq <lgqbang@gmail.com>
2018-05-25 15:09:51 +0700

    pull

--- app/src/main/java/vn/javis/tourde/model/Data.java
+++ app/src/main/java/vn/javis/tourde/model/Data.java
@@ -1,12 +1,31 @@
 package vn.javis.tourde.model;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Data implements Serializable {
 
     private String title;
     private List<String> content;
+    private boolean isMarked;
+    private int positionMarked;
+
+    public boolean isMarked() {
+        return isMarked;
+    }
+
+    public void setMarked(boolean marked) {
+        isMarked = marked;
+    }
+
+    public int getPositionMarked() {
+        return positionMarked;
+    }
+
+    public void setPositionMarked(int positionMarked) {
+        this.positionMarked = positionMarked;
+    }
 
     public String getTitle() {
         return title;
@@ -20,7 +39,10 @@ public class Data implements Serializable {
         return content;
     }
 
+
     public void setContent(List<String> content) {
         this.content = content;
     }
+
+
 }
