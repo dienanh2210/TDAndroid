dd47cb38e9dde45235e341bd6b2ea7ad94115adb
nguyen van dien <anhdien2210@gmail.com>
2018-06-07 14:25:50 +0700

    show

--- app/src/main/java/vn/javis/tourde/adapter/ListCommentAdapter.java
+++ app/src/main/java/vn/javis/tourde/adapter/ListCommentAdapter.java
@@ -49,12 +49,9 @@ public class ListCommentAdapter extends RecyclerView.Adapter<ListCommentAdapter.
     @Override
     public void onBindViewHolder(@NonNull ListCommentAdapter.CommentViewHolder holder, final int position) {
         Review model = listReview.get(position);
-
         if (model.getImage() != "" && model.getImage() != null)
             holder.txtUserName.setText(model.getNickname());
-        //  else holder.txtUserName.setText(model.getEmail());
         holder.txtCommentContent.setText(model.getComment());
-        //  holder.txtPostDate.setText(model.getPostDate().toString());
         String dateGet = model.getReviewUpdateDatetime();
         if (dateGet != null && dateGet != "") {
 
