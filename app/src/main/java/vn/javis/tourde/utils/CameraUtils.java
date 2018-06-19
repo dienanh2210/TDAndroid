b5ca4c815e447898aedf10fdbc727ca2298e963f
nvthanh <thanh.nguyen.viet@socialfintech.net>
2018-06-19 11:59:47 +0700

    [Android] http://redmine.javis.vn/redmine/issues/6764 (Fix bug)

--- app/src/main/java/vn/javis/tourde/utils/CameraUtils.java
+++ app/src/main/java/vn/javis/tourde/utils/CameraUtils.java
@@ -12,15 +12,22 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.Typeface;
+import android.hardware.Camera;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.MediaStore;
 import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import java.io.File;
@@ -75,6 +82,14 @@ public class CameraUtils {
         return b;
     }
 
+    public static Bitmap loadBitmapFromViewAuto(View v, int originalWidth, int originalHeight) {
+        Bitmap b = Bitmap.createBitmap(v.getWidth(), v.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(b);
+        v.draw(c);
+        return b;
+    }
+
+
     public static Bitmap optimizeBitmap(int sampleSize, String filePath) {
         // bitmap factory
         BitmapFactory.Options options = new BitmapFactory.Options();
@@ -124,6 +139,82 @@ public class CameraUtils {
         values.put(MediaStore.MediaColumns.DATA, filePath);
         context.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
 
+    }
+
+    public static Bitmap addLayout(Bitmap toEdit, String courseDesc, String distance, String spot, String time, String logo) {
+        Log.i(TAG, "addLayout: ");
+        Bitmap dest = toEdit.copy(Bitmap.Config.ARGB_8888, true);
+        int pictureHeight = dest.getHeight();
+        int pictureWidth = dest.getWidth();
+        int margin = 50;
+        int padding = 100;
+        int paddingLine = 200;
+
+
+        Canvas canvas = new Canvas(dest);
+        //Text Info Design
+        Paint painTextInfo = new Paint();  //set the look
+        painTextInfo.setAntiAlias(true);
+        painTextInfo.setColor(Color.WHITE);
+        painTextInfo.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD));
+        painTextInfo.setStyle(Paint.Style.FILL);
+        painTextInfo.setShadowLayer(2.0f, 1.0f, 1.0f, Color.GRAY);
+        painTextInfo.setTextSize(pictureHeight * .03f);
+
+        //Text Header design
+        Paint painTextHeader = new Paint();  //set the look
+        painTextHeader.setAntiAlias(true);
+        painTextHeader.setColor(Color.WHITE);
+        painTextHeader.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.NORMAL));
+        painTextHeader.setStyle(Paint.Style.FILL);
+        painTextHeader.setShadowLayer(2.0f, 1.0f, 1.0f, Color.GRAY);
+        painTextHeader.setTextSize(pictureHeight * .03f);
+
+        //Logo design
+        Paint painTextLogo = new Paint();  //set the look
+        painTextLogo.setAntiAlias(true);
+        painTextLogo.setColor(Color.WHITE);
+        painTextLogo.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD));
+        painTextLogo.setStyle(Paint.Style.FILL);
+        painTextLogo.setShadowLayer(2.0f, 1.0f, 1.0f, Color.GRAY);
+        painTextLogo.setTextSize(pictureHeight * .03f);
+
+        //Line design
+        Paint paintLine = new Paint();  //set the look
+        paintLine.setAntiAlias(true);
+        paintLine.setColor(Color.WHITE);
+        paintLine.setStyle(Paint.Style.FILL);
+        paintLine.setStrokeWidth(10f);
+        paintLine.setShadowLayer(2.0f, 1.0f, 1.0f, Color.GRAY);
+        //Logo
+        canvas.drawText(logo, pictureWidth * 3 / 4, 100, painTextLogo);
+        //Draw line 1
+        canvas.drawLine(padding, pictureHeight * 2 / 3 + margin, pictureWidth / 3, pictureHeight * 2 / 3 + margin, paintLine);
+        canvas.drawText("COURSE", padding, pictureHeight * 2 / 3, painTextHeader);
+        canvas.drawText(courseDesc, padding, pictureHeight * 2 / 3 + paddingLine, painTextHeader);
+        canvas.drawLine(pictureWidth * 2 / 3, pictureHeight * 2 / 3 + margin, pictureWidth - padding, pictureHeight * 2 / 3 + margin, paintLine);
+        canvas.drawText("SPOT", pictureWidth * 2 / 3, pictureHeight * 2 / 3, painTextHeader);
+        canvas.drawText(spot, pictureWidth * 2 / 3, pictureHeight * 2 / 3 + paddingLine, painTextHeader);
+
+        //Draw line 2
+        canvas.drawLine(padding, pictureHeight * 5 / 6 + margin, pictureWidth / 3, pictureHeight * 5 / 6 + margin, paintLine);
+        canvas.drawText("TIME", padding, pictureHeight * 5 / 6, painTextHeader);
+        canvas.drawText(time, padding, pictureHeight * 5 / 6 + paddingLine, painTextHeader);
+        canvas.drawLine(pictureWidth * 2 / 3, pictureHeight * 5 / 6 + margin, pictureWidth - padding, pictureHeight * 5 / 6 + margin, paintLine);
+        canvas.drawText("DISTANCE", pictureWidth * 2 / 3, pictureHeight * 5 / 6, painTextHeader);
+        canvas.drawText(distance, pictureWidth * 2 / 3, pictureHeight * 5 / 6 + paddingLine, painTextHeader);
+
+        return dest;
+    }
+
+    public static Bitmap addView(Bitmap bitmap, View v) {
+        Log.i(TAG, "addLayout: ");
+        Bitmap view = loadBitmapFromView(v);
+        Bitmap scaleView = Bitmap.createScaledBitmap(view, bitmap.getWidth(), bitmap.getHeight() , false);
+        Bitmap dest = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+        Canvas c = new Canvas(dest);
+        c.drawBitmap(scaleView, 0, 0, null);
+        return dest;
     }
 
 
