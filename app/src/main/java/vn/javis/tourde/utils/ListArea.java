da0a9f13cee975ceef8e8afcc522f08a2ab300c8
bang lq <lgqbang@gmail.com>
2018-07-19 16:56:02 +0700

    save

--- app/src/main/java/vn/javis/tourde/utils/ListArea.java
+++ app/src/main/java/vn/javis/tourde/utils/ListArea.java
@@ -12,7 +12,7 @@ public class ListArea {
         final String[] contentList4 = new String[]{"新潟県", "富山県", "石川県", "福井県", "長野県", "山梨県", "岐阜県", "静岡県", "愛知県"};
         final String[] contentList5 = new String[]{"三重県", "滋賀県", "京都府", "大阪府", "兵庫県", "奈良県", "和歌山県"};
         final String[] contentList6 = new String[]{"鳥取県", "島根県", "岡山県", "広島県", "山口県"};
-        final String[] contentList7 = new String[]{"徳島県", "香川県", "愛媛県", "愛媛県", "高知県"};
+        final String[] contentList7 = new String[]{"徳島県", "香川県", "愛媛県",  "高知県"};
         final String[] contentList8 = new String[]{"福岡県", "佐賀県", "長崎県", "熊本県", "大分県", "宮崎県", "鹿児島県", "沖縄県"};
         final ArrayList<String> listArea = new ArrayList<>();
         listArea.addAll(Arrays.asList(contentList1));
@@ -32,8 +32,11 @@ public class ListArea {
         final String[] contentList4 = new String[]{"新潟県", "富山県", "石川県", "福井県", "長野県", "山梨県", "岐阜県", "静岡県", "愛知県"};
         final String[] contentList5 = new String[]{"三重県", "滋賀県", "京都府", "大阪府", "兵庫県", "奈良県", "和歌山県"};
         final String[] contentList6 = new String[]{"鳥取県", "島根県", "岡山県", "広島県", "山口県"};
-        final String[] contentList7 = new String[]{"徳島県", "香川県", "愛媛県", "愛媛県", "高知県"};
+        final String[] contentList7 = new String[]{"徳島県", "香川県", "愛媛県",  "高知県"};
         final String[] contentList8 = new String[]{"福岡県", "佐賀県", "長崎県", "熊本県", "大分県", "宮崎県", "鹿児島県", "沖縄県"};
+
+;
+
         final ArrayList<String> listArea = new ArrayList<>();
         listArea.addAll(Arrays.asList(contentList1));
         listArea.addAll(Arrays.asList(contentList2));
