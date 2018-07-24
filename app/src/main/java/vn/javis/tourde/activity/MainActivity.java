fdb588c4e17436c742317022ff9d5d4a6466d777
nguyen van dien <anhdien2210@gmail.com>
2018-07-24 14:53:24 +0700

    fix ...

--- app/src/main/java/vn/javis/tourde/activity/MainActivity.java
+++ app/src/main/java/vn/javis/tourde/activity/MainActivity.java
@@ -3,15 +3,20 @@ package vn.javis.tourde.activity;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
@@ -68,7 +73,6 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("GrowthMessage", error);
             }
         });
-
 
     }
 
